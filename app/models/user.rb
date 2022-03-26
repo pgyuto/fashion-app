f855fa9ac0b378ee5f2194c90143eccc6c77f67b
pgyuto <pgyuto0235@gmail.com>
2022-03-26 10:41:01 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :nickname, presence: true
 end
