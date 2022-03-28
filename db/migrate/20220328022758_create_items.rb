bde1b5a3a8960a6b1f6d6f6e0c25d20a6cf2e0b1
pgyuto <pgyuto0235@gmail.com>
2022-03-28 11:50:09 +0900

    item テーブル作成

--- db/migrate/20220328022758_create_items.rb
+++ db/migrate/20220328022758_create_items.rb
@@ -1,7 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.text :text, null:false
+      t.integer :category_id, null:false
+      t.integer :color_id, null:false
+      t.string :maker
+      t.references :user, null:false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_092049) do
+ActiveRecord::Schema.define(version: 2022_03_28_022758) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.integer "category_id", null: false
+    t.integer "color_id", null: false
+    t.string "maker"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2022_03_25_092049) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
