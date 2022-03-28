55427d47fef5fddcd2d38168021e00e66b142d52
pgyuto <pgyuto0235@gmail.com>
2022-03-28 12:06:58 +0900

    簡易トップページ作成

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  before_action :move_to_item, only: :index
   def index
+  end
+
+
+  private
+  def move_to_item
+    if user_signed_in?
+      redirect_to items_path
+    end
   end
 end
