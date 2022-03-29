0a87e555b2baa3b979836c4b9e0f48e22b04dd62
pgyuto <pgyuto0235@gmail.com>
2022-03-29 11:01:30 +0900

    itrem new createルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "pages#index"
   devise_for :users
   resources :pages, only: :index
-  resources :items, only: :index
+  resources :items, only: [:index, :new, :create]
 end
