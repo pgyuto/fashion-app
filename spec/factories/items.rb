5a56c324a40549b21d28ec36e3ccf508e76219c3
pgyuto <pgyuto0235@gmail.com>
2022-03-29 14:45:13 +0900

    item テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    text { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 2, to: 6) }
+    color_id { Faker::Number.between(from: 2, to: 15) }
+    maker { Faker::Lorem.sentence }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '服の登録機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+    
+    context '登録できるとき' do
+      it '全ての値が正しく入力できれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'メーカーの値が空でも登録できる' do
+        @item.maker = ''
+        expect(@item).to be_valid
+      end
+    end
+
+    context '登録できないとき' do
+      it 'text:必須' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'category_id:必須' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'image:必須' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'color_id:必須' do
+        @item.color_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Color can't be blank")
+      end
+      it 'ユーザーが紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it 'category_idが1だと登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'color_idが1だと登録できない' do
+        @item.color_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Color can't be blank")
+      end
+    end
+  end
 end
