d68ca26521e32522458fad90efb2299277d163f5
pgyuto <pgyuto0235@gmail.com>
2022-03-29 11:07:53 +0900

    item,user バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :text
+    validates :category_id
+    validates :color_id
+  end
+
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX }
   validates :nickname, presence: true
+
+  has_many :items
 end
