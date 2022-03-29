d32d783fde8cb0cee6fa2e6ebbd8acc92ce9fd0a
pgyuto <pgyuto0235@gmail.com>
2022-03-29 12:14:59 +0900

    ストロングパラメータ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,19 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :text, :category_id, :color_id, :image
+    ).merge(user_id: current_user.id)
+  end
 end
