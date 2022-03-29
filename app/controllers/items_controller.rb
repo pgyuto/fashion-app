2f286ee5b54258d6a661098636239ca308106747
pgyuto <pgyuto0235@gmail.com>
2022-03-29 12:04:22 +0900

    簡易的なビューの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,12 @@ class ItemsController < ApplicationController
   def index
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    binding.pry
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :color
 
-    validates :text, presence: true
-    validates :category_id, :color_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :text, presence: true
+  validates :category_id, :color_id, numericality: { other_than: 1, message: "can't be blank"} 
 
   belongs_to :user
 end
