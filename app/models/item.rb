c23eeecfc5070eb67402edfa8da7bb896ad950bc
pgyuto <pgyuto0235@gmail.com>
2022-03-29 11:38:26 +0900

    item バリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :color
 
-  
-  with_options presence: true do
-    validates :text
-    validates :category_id
-    validates :color_id
-  end
-
+    validates :text, presence: true
+    validates :category_id, :color_id, numericality: { other_than: 1, message: "can't be blank"} 
 
   belongs_to :user
 end
