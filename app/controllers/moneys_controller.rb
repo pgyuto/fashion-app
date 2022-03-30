909cc9f87958fe6401a71961cb740677134aa0d8
pgyuto <pgyuto0235@gmail.com>
2022-03-30 19:51:40 +0900

    使用金額登録機能実装

--- app/controllers/moneys_controller.rb
+++ app/controllers/moneys_controller.rb
@@ -1,4 +1,25 @@
 class MoneysController < ApplicationController
   def index
   end
+
+  def new
+    @money = Money.new
+  end
+
+  def create
+    @money = Money.new(item_params)
+    if @money.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:money).permit(
+      :balance
+    ).merge(user_id: current_user.id)
+  end
 end
