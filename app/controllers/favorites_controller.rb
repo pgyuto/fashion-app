9eca03f9f63429d6a1520ae69c7c9f054d230edd
pgyuto <pgyuto0235@gmail.com>
2022-04-20 20:43:18 +0900

    favorites コントローラー追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,9 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
