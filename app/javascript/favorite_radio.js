a64d746e79bc081364022a7a6f23e1c7db0de7ef
pgyuto <pgyuto0235@gmail.com>
2022-04-30 14:04:55 +0900

    検索ラジオボタン選択解除実装

--- app/javascript/favorite_radio.js
+++ app/javascript/favorite_radio.js
@@ -1,5 +1,17 @@
 function favorite_radio (){
-  console.log("イベント発火")
+  const favoriteBtn = document.getElementById('favorite-btn');
+  favoriteBtn.addEventListener('click',() => {
+    let state = favoriteBtn.dataset.status;
+    //data-statusが1で、ラジオボタンがchecked状態で解除
+    if( state == "1" && favoriteBtn.checked == true ){
+      favoriteBtn.checked = false;
+      favoriteBtn.dataset.status = "0";
+    } else {
+      //data-statusが0の場合、ラジオボタンがchecked、data-statusを1に変更
+      favoriteBtn.checked = true;
+      favoriteBtn.dataset.status = "1";
+    };
+  });
 };
 
 
